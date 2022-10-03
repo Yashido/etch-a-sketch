968c6129a652eeb3f003d4c7b002f9d2fb6f1150
Yashido <jbpablico27@gmail.com>
2022-10-02 23:58:50 -0500

    Created a grid element in HTML. Began work on Javascript file.

---
+++ script.js
@@ -0,0 +1,7 @@
+const colorPicker = document.getElementById('colorPicker');
+const colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const resetBtn = document.getElementById('eraserBtn');
+const sideValue = document.getElementById('sideValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const cnt_grid = document.getElementById('cnt-grid');
