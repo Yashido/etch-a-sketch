250d79e7995766ef440c768142bd993f397c5e49
wimllie <jbpablico27@gmail.com>
2022-10-04 16:56:45 -0500

    Added the grid function and ccs elements

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const resetBtn = document.getElementById('eraserBtn');
 const sideValue = document.getElementById('sideValue');
 const sizeSlider = document.getElementById('sizeSlider');
 const cnt_grid = document.getElementById('cnt-grid');
+
+function setGrid(size) {
+    cnt_grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    cnt_grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++) {
+        
+    }
+
+}
