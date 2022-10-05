d2ff07312affc16deef27f2eea8d8b751d6bedb8
wimllie <jbpablico27@gmail.com>
2022-10-05 01:14:33 -0500

    Need to rework the logic in javascript

--- script.js
+++ script.js
@@ -11,7 +11,30 @@ function setGrid(size) {
     cnt_grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for(let i = 0; i < size * size; i++) {
-        
+        const cnt_gridEl = document.createElement('div');
+        cnt_gridEl.classList.add('gridElement');
+        cnt_gridEl.addEventListener('mouseover', changeColor);
+        cnt_gridEl.addEventListener('mousedown', changeColor);
+        cnt_grid.appendChild(cnt_gridEl);
     }
 
+}
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
+
+const gridSize = 16;
+
+window.onload = () => {
+    setGrid(gridSize);
 }
