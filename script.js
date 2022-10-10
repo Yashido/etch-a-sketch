2abe88fc9656a5da96ac474531a8f24ed744b4d8
Yashido <jbpablico27@gmail.com>
2022-10-09 23:39:57 -0500

    Added cleargrid function

--- script.js
+++ script.js
@@ -13,22 +13,10 @@ const value = document.getElementById('sizeSlider').oninput = () => setSizeValue
 function setSizeValue(value) {
   sizeValue.innerHTML = `${value} x ${value}`
 }
-/*
-function setGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for(let i = 0; i < size * size; i++) {
-        const gridEl = document.createElement('div');
-        gridEl.classList.add('gridElement');
-        gridEl.addEventListener('mouseover', changeColor);
-        gridEl.addEventListener('mousedown', changeColor);
-        grid.appendChild(gridEl);
-    }
-
+function clearGrid() {
+  grid.innerHTML = '';
 }
-*/
-
 function setGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -38,21 +26,7 @@ function setGrid(size) {
       grid.appendChild(gridDiv);
     }
 }
-/*
-function changeColor(e) {
-    if (e.type === 'mouseover' && !mouseDown) return
-    if (currentMode === 'rainbow') {
-      const randomR = Math.floor(Math.random() * 256)
-      const randomG = Math.floor(Math.random() * 256)
-      const randomB = Math.floor(Math.random() * 256)
-      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-    } else if (currentMode === 'color') {
-      e.target.style.backgroundColor = currentColor
-    } else if (currentMode === 'eraser') {
-      e.target.style.backgroundColor = '#fefefe'
-    }
-  }
-*/
+
 const gridSize = 16;
 
 /*
