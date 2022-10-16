629fa89aa5fa0eff340d272875c80f9032ebd2fc
Yashido <jbpablico27@gmail.com>
2022-10-16 03:16:23 -0500

    commented outunfinshed code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const DEFAULT_VALUE = '#69a2ff';
 const DEFAULT_SIZE = 16;
-const DEFAULT_MODE = colorBtn;
+//const DEFAULT_MODE = colorBtn;
 
 let crtValue = DEFAULT_VALUE;
 let crtSize = DEFAULT_SIZE;
-let crtMode = DEFAULT_MODE;
+// let crtMode = DEFAULT_MODE;
 
 const colorPicker = document.getElementById('colorPicker');
 const colorBtn = document.getElementById('colorBtn');
@@ -13,17 +13,20 @@ const resetBtn = document.getElementById('eraserBtn');
 const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById('sizeSlider');
 const grid = document.getElementById('grid');
-const mouseValue;
+// const mouseValue;
 const value = document.getElementById('sizeSlider').oninput = () => setSizeValue(value);
 
 function setSizeValue(value) {
   sizeValue.innerHTML = `${value} x ${value}`
 }
+/*
 function setMode(colorBtn,rainbowBtn) {
   const mouseValue.document.oninput();
-};
+}
+*/
 function clearGrid() {
   grid.innerHTML = '';
+
 }
 function setGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
